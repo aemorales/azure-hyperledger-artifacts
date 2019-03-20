84c409e290e1831a5f1e908effd88f7d3b6337ec
aemorales <amorales@oikos.financial>
2019-03-20 15:29:11 -0400

    Update run_tutorial_template.sh

--- tutorial/run_tutorial_template.sh
+++ tutorial/run_tutorial_template.sh
@@ -26,10 +26,10 @@ cp ~/crypto-config/peerOrganizations/{{PEER_ORG_DOMAIN}}/users/Admin@{{PEER_ORG_
 cp ~/crypto-config/peerOrganizations/{{PEER_ORG_DOMAIN}}/users/Admin@{{PEER_ORG_DOMAIN}}/msp/keystore/* Admin@{{PEER_ORG_DOMAIN}}_sk
 
 composer card create -p connection.json -u PeerAdmin -c Admin@{{PEER_ORG_DOMAIN}}-cert.pem -k Admin@{{PEER_ORG_DOMAIN}}_sk -r PeerAdmin -r ChannelAdmin
-composer card import -f PeerAdmin@tutorial-network.card
-composer network install -c PeerAdmin@tutorial-network -a tutorial-network@0.0.1.bna
+composer card import -f PeerAdmin@trustt-network.card
+composer network install -c PeerAdmin@trustt-network -a trustt-network@0.0.1.bna
 
-composer network start --networkName tutorial-network --networkVersion 0.0.1 -A {{CA_USER}} -S {{CA_PASSWORD}} -c PeerAdmin@tutorial-network
-composer card import -f admin@tutorial-network.card
-composer network ping -c admin@tutorial-network
+composer network start --networkName trustt-network --networkVersion 0.0.1 -A {{CA_USER}} -S {{CA_PASSWORD}} -c PeerAdmin@trustt-network
+composer card import -f admin@trustt-network.card
+composer network ping -c admin@trustt-network
 
